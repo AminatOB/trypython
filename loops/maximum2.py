2236e3a112b14a32b7c3c277f9a3ae1699b300b1
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-05-02 16:01:35 +0000

    practice

--- loops/maximum2.py
+++ loops/maximum2.py
@@ -1,5 +1,5 @@
 largest = None
-for itervar in [3, 41, 12, 9, 74, 15]:
+for itervar in [3, 74, 12, 9, 41, 15]:
     if largest is None or itervar > largest:
         largest = itervar
         print('Largest:', largest)
