9f3aab613de215a38079d5cced6787018fade853
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-05-07 14:59:17 +0000

    corrected

--- constructed/timegreet.py
+++ constructed/timegreet.py
@@ -1,13 +1,13 @@
 #program that accepts time in 24hours format and then greet the user#
 
 time = int(input('Enter your time:'))
-if time >= 0 & time <= 12:
+if time >= 0 and time <= 12:
     print('Good morning')
-elif time >= 13 & time <= 16:
+elif time >= 13 and time <= 16:
     print('Good afternoon')
-elif time >= 17 & time <= 20:
+elif time >= 17 and time <= 20:
     print('Good evening')
-elif time >= 21 & time <= 24:
+elif time >= 21 and time <= 24:
     print('Good night')
 else:
     print('wrong input')
