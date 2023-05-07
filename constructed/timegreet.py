f470e7a7d254d84008d90dacf3d738891ef0a079
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-05-07 15:20:09 +0000

    updated

--- constructed/timegreet.py
+++ constructed/timegreet.py
@@ -12,3 +12,25 @@ elif time >= 21 and time <= 24:
 else:
     print('wrong input')
 
+
+
+
+#2#
+time = int(input('Enter your time:'))
+
+while time > 0:
+    if time >= 0 and time <= 12:
+        print('Good morning')
+        break
+    elif time >= 13 and time <= 16:
+        print('Good afternoon')
+        break
+    elif time >= 17 and time <= 20:
+        print('Good evening')
+        break
+    elif time >= 21 and time <= 24:
+        print('Good night')
+        break
+    else:
+        time = int(input('Enter your time:'))
+
