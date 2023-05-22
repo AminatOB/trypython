ae176f3f35ed157c44771ebeb359b9d09db8e5f6
Bakare Aminat Oluwatoyosi <aminatbkr@gmail.com>
2023-05-22 21:17:29 +0000

    practice

---
+++ constructed/keynval.py
@@ -0,0 +1,9 @@
+#program to  create two dictionary objects named states and cities,addition add more two items to the cities dictionary. Then print the details available in cities dictionary using keys and values.#
+
+states = {'Oregon':'OR', 'Florida':'FL', 'California':'CA', 'New York':'NY', 'Michigan':'MI'}
+cities = {'CA':'San Fransisco', 'MI':'Detroit', 'FL':'Jacksonville'}
+cities ['NY'] = 'New York'
+cities ['OR'] = 'Portland'
+for all in cities:
+    print(all, cities[all])
+
